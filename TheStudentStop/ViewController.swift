0149342306d14a4a55866a79c75989abefb7cf99
Ndiana Akpan <ndianaakpan19@gmail.com>
2022-08-02 20:11:05 +0200

    Initial Commit

---
+++ TheStudentStop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheStudentStop
+//
+//  Created by Ndiana Akpan on 8/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
