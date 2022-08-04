45b787d5d1a85dddb5bae8fe397bcb70f40ae800
Ndiana Akpan <ndianaakpan19@gmail.com>
2022-08-04 21:20:59 +0200

    added code for question one and two and designed the buttons and added labels

--- TheStudentStop/SurveyViewController.swift
+++ TheStudentStop/SurveyViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class SurveyViewController: UIViewController {
 
     @IBOutlet weak var Q1: UILabel!
+    
+    @IBOutlet weak var Q2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -28,10 +31,60 @@ class SurveyViewController: UIViewController {
     */
 
     
+  
     @IBAction func Button1(_ sender: Any) {
-            Q1.text = "Get at least 8 hours of sleep"
+        Q1.text = "Go watch a movie."
+    }
+    
+    @IBAction func Button2(_ sender: Any) {
+        Q1.text = "Get walking or biking for an hour."
+    }
+    
+    @IBAction func Button3(_ sender: Any) {
+        Q1.text = "Read a book for 45 minutes."
+    }
+    
+    @IBAction func Button4(_ sender: Any) {
+        Q1.text = "Meditate for 20 minutes."
+    }
+    
+    @IBAction func Button5(_ sender: Any) {
+        Q1.text = "Take a nap for 30 minutes"
     }
     
     
+    @IBAction func secondButton1(_ sender: Any) {
+        Q2.text = "Take a bubblebath."
+    }
+    
+    @IBAction func secondButton2(_ sender: Any) {
+        Q2.text = "Go eat your favorite food."
+    }
+    
+    @IBAction func secondButton3(_ sender: Any) {
+        Q2.text = "Try something new."
+    }
+    
+    @IBAction func secondButton4(_ sender: Any) {
+        Q2.text = "Have a self-care day."
+    }
+    
+    @IBAction func secondButton5(_ sender: Any) {
+        Q2.text = "Go take a nap."
+    }
+    
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
